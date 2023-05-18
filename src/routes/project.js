9996c8550e02bbfcb55dd6a32afde4387ba3fe28
Diwakar38 <diwakar.arya1999@gmail.com>
2023-05-18 20:25:33 +0530

    Creation of Dynamic components

--- src/routes/project.js
+++ src/routes/project.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
+import HeroImg2 from "../components/heroImg2";
 
 const Project = () => {
     return (
         <div>
             <Navbar />
+            <HeroImg2 heading="PROJECTS." text="Some of my most recent works" />
             <Footer />
         </div>
     )
